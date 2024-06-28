4be540bebe5c28e77656607f51dd3fe064cf5acd
amber-charitos <67629430+AmberCharitos@users.noreply.github.com>
2024-06-28 11:01:53 +0100

    CSS-8935 Use juju secrets for catalog config relation data (#47)
    * add trino relation skeleton
    
    * add unit and integration tests and finalise trino handler
    
    * split unit test files
    
    * split worker and coordinator classes and fix idle status
    
    * add worker relation back on charm crash
    
    * address review comments
    
    * update loki lib
    
    * adds juju secret usage for config
    
    * add nosec for secret_label literal
    
    * remove unneccessary if/else

--- src/literals.py
+++ src/literals.py
@@ -49,6 +49,8 @@ JAVA_ENV = {
     "JAVA_HOME": "/usr/lib/jvm/java-21-openjdk-amd64",
 }
 
+SECRET_LABEL = "catalog-config"  # nosec
+
 # Connector literal
 CONNECTOR_FIELDS = {
     "elasticsearch": {
