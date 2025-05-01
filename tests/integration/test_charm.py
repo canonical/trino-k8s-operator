6e8a2caab5951bc37816cdba2881c078eb245ecb
Ali Kelkawi <ali.kelkawi@canonical.com>
2025-05-01 17:06:30 +0300

    remove redundant code blocks

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -5,10 +5,8 @@
 """Trino charm integration tests."""
 
 import logging
-from pathlib import Path
 
 import pytest
-import pytest_asyncio
 from conftest import deploy  # noqa: F401, pylint: disable=W0611
 from helpers import (
     APP_NAME,
@@ -20,33 +18,9 @@ from helpers import (
     simulate_crash_and_restart,
     update_catalog_config,
 )
-from pytest import FixtureRequest
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
-
-
-@pytest.fixture(scope="module", name="charm_image")
-def charm_image_fixture(request: FixtureRequest) -> str:
-    """The OCI image for charm."""
-    charm_image = request.config.getoption("--trino-image")
-    assert (
-        charm_image
-    ), "--trino-image argument is required which should contain the name of the OCI image."
-    return charm_image
-
-
-@pytest_asyncio.fixture(scope="module", name="charm")
-async def charm_fixture(
-    request: FixtureRequest, ops_test: OpsTest
-) -> str | Path:
-    """Fetch the path to charm."""
-    charms = request.config.getoption("--charm-file")
-    if not charms:
-        charm = await ops_test.build_charm(".")
-        assert charm, "Charm not built"
-        return charm
-    return charms[0]
 
 
 @pytest.mark.abort_on_fail
--- tests/integration/test_policy.py
+++ tests/integration/test_policy.py
@@ -6,7 +6,6 @@
 
 import logging
 import time
-from pathlib import Path
 
 import pytest
 import pytest_asyncio
@@ -21,34 +20,9 @@ from helpers import (
     get_unit_url,
     update_policies,
 )
-from pytest import FixtureRequest
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
-
-
-@pytest.fixture(scope="module", name="charm_image")
-def charm_image_fixture(request: FixtureRequest) -> str:
-    """The OCI image for charm."""
-    charm_image = request.config.getoption("--trino-image")
-    assert (
-        charm_image
-    ), "--trino-image argument is required which should contain the name of the OCI image."
-    return charm_image
-
-
-@pytest_asyncio.fixture(scope="module", name="charm")
-async def charm_fixture(
-    request: FixtureRequest, ops_test: OpsTest
-) -> str | Path:
-    """Fetch the path to charm."""
-    charms = request.config.getoption("--charm-file")
-    if not charms:
-        charm = await ops_test.build_charm(".")
-        assert charm, "Charm not built"
-        return charm
-    return charms[0]
-
 
 TRINO_CONFIG = {
     "ranger-service-name": "trino-service",
