9eb1d9ae754d7eb6bd36247a3826ccb758fedc7c
Ali Kelkawi <81743070+kelkawi-a@users.noreply.github.com>
2026-03-11 15:40:52 +0300

    Add config parameter for max-run-time (#106)
    * add config parameter for max run time

--- src/charm.py
+++ src/charm.py
@@ -633,6 +633,7 @@ class TrinoK8SCharm(CharmBase):
             "WORKER_REQUEST_TIMEOUT": self.config["worker-request-timeout"],
             "MAX_CONCURRENT_QUERIES": self.config["max-concurrent-queries"],
             "QUERY_MAX_CPU_TIME": self.config.get("query-max-cpu-time"),
+            "QUERY_MAX_RUN_TIME": self.config.get("query-max-run-time"),
             "QUERY_MAX_MEMORY_PER_NODE": self.config.get(
                 "query-max-memory-per-node"
             ),
